4b0e6a059d17e7506f90e654655be3738a75089d
Dimitris Rozakis <dimrozakis@gmail.com>
2016-10-22 18:14:00 +0300

    Add sort, count, and strip_dirs params in stats view

--- tornado_profile.py
+++ tornado_profile.py
@@ -69,8 +69,31 @@ class ProfileStatsHandler(tornado.web.RequestHandler):
 
     def get(self):
         """Return current profiler statistics."""
+
+        sort = self.get_argument('sort', 'cum_time')
+        count = self.get_argument('count', 20)
+        strip_dirs = self.get_argument('strip_dirs', True)
+        error = ''
+        sorts = ('num_calls', 'cum_time', 'total_time',
+                 'cum_time_per_call', 'total_time_per_call')
+        if sort not in sorts:
+            error += "Invalid `sort` '%s', must be in %s." % (sort, sorts)
         try:
-            statistics = get_profiler_statistics()
+            count = int(count)
+        except (ValueError, TypeError):
+            error += "Can't cast `count` '%s' to int." % count
+        if count <= 0:
+            count = None
+        strip_dirs = str(strip_dirs).lower() not in ('false', 'no', 'none',
+                                                     'null', '0', '')
+        if error:
+            self.write({'error': error})
+            self.set_status(400)
+            self.finish()
+            return
+
+        try:
+            statistics = get_profiler_statistics(sort, count, strip_dirs)
             self.write({'statistics': statistics})
             self.set_status(200)
         except TypeError:
